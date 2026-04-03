dd9c9967d7c1d7b8a93f719cb69082ccae885c93
Omar Anwar <anwrmr649@gmail.com>
2026-04-03 16:36:59 +0200

    add scroll buttons to carousel

--- src/components/HeroCarousel.tsx
+++ src/components/HeroCarousel.tsx
@@ -5,6 +5,8 @@ import {
   CarouselContent,
   CarouselItem,
 } from "./ui/carousel";
+import { Button } from "./ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 function HeroCarousel({ images }: { images: ComponentProps<"img">[] }) {
   const [api, setApi] = useState<CarouselApi>();
@@ -23,13 +25,27 @@ function HeroCarousel({ images }: { images: ComponentProps<"img">[] }) {
           ))}
         </CarouselContent>
       </Carousel>
-      <div className="flex justify-center gap-2 p-3">
+      <div className="flex items-center justify-center gap-2 p-3">
+        <Button
+          onClick={() => api?.scrollPrev()}
+          variant={"ghost"}
+          size={"icon-xs"}
+        >
+          <ChevronLeft />
+        </Button>
         {images.map((_, index) => (
           <div
             key={index}
             className={`${index === selected && "bg-foreground"} border-foreground h-2 w-2 rounded-full border`}
           />
         ))}
+        <Button
+          onClick={() => api?.scrollNext()}
+          variant={"ghost"}
+          size={"icon-xs"}
+        >
+          <ChevronRight />
+        </Button>
       </div>
     </>
   );
