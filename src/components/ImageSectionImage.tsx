06febf764228b59fb4a50b85c7791ce7db1f5669
Omar Anwar <anwrmr649@gmail.com>
2026-04-09 13:37:11 +0200

    add fixed image parallax thingy sections

---
+++ src/components/ImageSectionImage.tsx
@@ -0,0 +1,34 @@
+import { motion, useScroll, useTransform } from "motion/react";
+import type { GetImageResult } from "astro";
+import { useRef } from "react";
+
+function ImageSectionImage({
+  imageResult,
+  alt,
+}: {
+  imageResult: GetImageResult;
+  alt: string;
+}) {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 2]);
+  return (
+    <>
+      <div className="absolute inset-0" ref={ref}></div>
+      <motion.img
+        className="fixed top-0 h-svh object-cover"
+        src={imageResult.src}
+        alt={alt}
+        {...imageResult.attributes}
+        style={{
+          scale,
+        }}
+      />
+    </>
+  );
+}
+
+export default ImageSectionImage;
