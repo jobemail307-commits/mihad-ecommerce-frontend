c6ad37b4fdb1b16f2eef35f1cbb0b732ffe0df31
Omar Anwar <anwrmr649@gmail.com>
2026-04-10 16:15:59 +0200

    make navbar change when user is not on top of page

---
+++ src/components/NavbarWrapper.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState, type ReactNode } from "react";
+
+function NavbarWrapper({ children }: { children: ReactNode }) {
+  const [isOnTop, setIsOnTop] = useState(true);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setIsOnTop(window.scrollY <= 30);
+    });
+  }, []);
+  console.log(isOnTop);
+  return (
+    <div
+      className="data-[top=true]:bg-primary/57 text-primary-foreground group fixed inset-x-0 z-1 bg-transparent backdrop-blur-md transition-colors"
+      data-top={isOnTop}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default NavbarWrapper;
